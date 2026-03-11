dea63cc2646fd50672bdc355daef7257730d409b
PajorVi <vinicius.marques@sptech.school>
2026-03-11 17:35:37 -0300

    MostOrderedCard styles

--- carambolo-app/components/molecules/MostOrderedCard/MostOrderedCard.jsx
+++ carambolo-app/components/molecules/MostOrderedCard/MostOrderedCard.jsx
@@ -1,40 +1,30 @@
-import { Image, Text, View } from "react-native"
-import styles from "./MostOrderedCard.styles"
+import { Image, Text, View } from "react-native";
+import styles from "./MostOrderedCard.styles";
 
 const MostOrderedCard = ({ cardTitle, quantity, amount }) => {
     return (
         <View style={styles.cardContainer}>
-            <Image src={require("../../../assets/bolo.png")} style={{ width: "100%", height: 100 }} />
-
+            <Image
+                source={require("../../../assets/bolo.png")}
+                style={styles.image}
+            />
             <View style={styles.infoContainer}>
                 <Text style={styles.cardTitle}>
                     {cardTitle}
                 </Text>
                 <View style={styles.footer}>
                     <View style={styles.quantity}>
-                        <Text>
-                            Quantidade
-                        </Text>
-                        <Text>
-                            {quantity}
-                        </Text>
+                        <Text style={{fontWeight: "bold"}}>Quantidade</Text>
+                        <Text>{quantity}</Text>
                     </View>
                     <View style={styles.amount}>
-                        <Text>
-                            Valor
-                        </Text>
-                        <Text>
-                            R${amount}
-                        </Text>
+                        <Text style={{fontWeight: "bold"}}>Valor</Text>
+                        <Text>R${amount}</Text>
                     </View>
                 </View>
             </View>
-
-
-
-
         </View>
-    )
-}
+    );
+};
 
-export default MostOrderedCard
+export default MostOrderedCard;
--- carambolo-app/components/molecules/MostOrderedCard/MostOrderedCard.styles.js
+++ carambolo-app/components/molecules/MostOrderedCard/MostOrderedCard.styles.js
@@ -2,12 +2,40 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     cardContainer: {
-        display: 'flex',
+        flexDirection: "row",
+        alignItems: "stretch",
+        padding: 12,
+        columnGap: 12,
+    },
+    image: {
+        width: 140,
+        height: 160,
+        borderRadius: 8,
+        borderColor: "#A47032",
+        borderWidth: 2,
+        borderStyle: "solid",
     },
     infoContainer: {
-        // display: 'flex',
-        // flexDirection:
-    }
-})
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "space-between",
+    },
+    cardTitle: {
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    footer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 8,
+    },
+    quantity: {
+        flexDirection: "column",
+    },
+    amount: {
+        flexDirection: "column",
+        alignItems: "flex-end",
+    },
+});
 
-export default styles
+export default styles;
