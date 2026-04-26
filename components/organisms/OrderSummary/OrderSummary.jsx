99ab34c1e73db9544fde919fc782b596a3f2a52d
PajorVi <vinicius.marques@sptech.school>
2026-04-26 18:57:15 -0300

    fix modal oppening

--- components/organisms/OrderSummary/OrderSummary.jsx
+++ components/organisms/OrderSummary/OrderSummary.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
-import { Pressable, ScrollView, Text, View } from "react-native"
+import { Pressable, ScrollView, Text, View, Animated } from "react-native"
+import { useEffect, useRef } from "react"
 import styles from "./OrderSummary.styles"
 
 const formatDate = (dateString) => {
@@ -9,96 +10,81 @@ const formatDate = (dateString) => {
 };
 
 const OrderSummary = ({ onClose, order }) => {
+    const backdropOpacity = useRef(new Animated.Value(0)).current;
+    const slideY = useRef(new Animated.Value(600)).current;
+
+    useEffect(() => {
+        Animated.parallel([
+            Animated.timing(backdropOpacity, {
+                toValue: 1,
+                duration: 220,
+                useNativeDriver: true,
+            }),
+            Animated.timing(slideY, {
+                toValue: 0,
+                duration: 320,
+                useNativeDriver: true,
+            }),
+        ]).start();
+    }, [backdropOpacity, slideY]);
 
     return (
+        <View style={{ flex: 1, justifyContent: "flex-end" }}>
 
-        <View style={{ backgroundColor: "rgba(0, 0, 0, 0.75)", flex: 1, justifyContent: "flex-end" }}>
-
-            <Pressable
+            <Animated.View
                 style={{
                     position: "absolute",
                     width: "100%",
-                    height: "100%"
+                    height: "100%",
+                    backgroundColor: "rgba(0, 0, 0, 0.75)",
+                    opacity: backdropOpacity,
                 }}
-                onPress={onClose}
-            />
+            >
+                <Pressable style={{ width: "100%", height: "100%" }} onPress={onClose} />
+            </Animated.View>
 
-            <View style={styles.header}>
+            <Animated.View style={{ transform: [{ translateY: slideY }], position: "absolute", bottom: 0, width: "100%" }}>
 
-                <Pressable onPress={onClose} style={{ backgroundColor: "#A47032", width: 100, height: 5, borderRadius: 25 }} >
-                    {/* <Text style={{ color: "white", textAlign: "center" }}>Fechar</Text> */}
-                </Pressable>
+                <View style={styles.header}>
 
-                <Text style={styles.headerTitle}>Número do Pedido: {order?.id}</Text>
+                    <Pressable onPress={onClose} style={{ backgroundColor: "#A47032", width: 100, height: 5, borderRadius: 25 }} >
+                        {/* <Text style={{ color: "white", textAlign: "center" }}>Fechar</Text> */}
+                    </Pressable>
 
-                <Text style={styles.headerText}>Bolo de Cenoura c/ cobertura de Chocolate</Text>
+                    <Text style={styles.headerTitle}>Número do Pedido: {order?.id}</Text>
 
-            </View>
-
-            <ScrollView style={styles.body} contentContainerStyle={{ paddingBottom: 50 }}>
-
-                <View>
-
-                    <Text style={styles.title}>Montagem</Text>
-
-                    <View style={{ flexDirection: "row", gap: 20, width: "90%", flexWrap: "wrap" }}>
-                        <Text style={styles.label}>Tamanho: <Text style={styles.data}>{order?.bolo?.tamanho}</Text></Text>
-
-                        <Text style={styles.label}>Formato: <Text style={styles.data}>{order?.bolo?.formato}</Text></Text>
-
-                        <Text style={styles.label}>Massa: <Text style={styles.data}>{order?.bolo?.massa?.sabor}</Text></Text>
-
-                        <Text style={[styles.label, { width: "40%" }]}>Recheio: <Text style={styles.data}>{order?.bolo?.recheioPedido?.sabor1} e {order?.bolo?.recheioPedido?.sabor2}</Text></Text>
-                    </View>
+                    <Text style={styles.headerText}>Bolo de Cenoura c/ cobertura de Chocolate</Text>
 
                 </View>
 
-                <View>
-
-                    <Text style={styles.title}>Decoração</Text>
+                <ScrollView style={styles.body} contentContainerStyle={{ paddingBottom: 50 }}>
 
                     <View>
-                        <Text style={styles.data}>Nenhuma imagem de referência adicionada</Text>
 
-                        <Text style={[styles.label, { paddingTop: 20 }]}>Observações</Text>
+                        <Text style={styles.title}>Montagem</Text>
 
-                        <Text style={styles.data}>{order?.observacao}</Text>
+                        <View style={{ flexDirection: "row", gap: 20, width: "90%", flexWrap: "wrap" }}>
+                            <Text style={styles.label}>Tamanho: <Text style={styles.data}>{order?.bolo?.tamanho}</Text></Text>
 
-                    </View>
+                            <Text style={styles.label}>Formato: <Text style={styles.data}>{order?.bolo?.formato}</Text></Text>
 
-                </View>
+                            <Text style={styles.label}>Massa: <Text style={styles.data}>{order?.bolo?.massa?.sabor}</Text></Text>
 
-                <View>
-
-                    <Text style={styles.title}>Adicionais</Text>
-
-                    <View>
-                        <Text style={styles.data}>Checks</Text>
-                    </View>
-
-                </View>
-
-                <View>
-
-                    <Text style={styles.title}>Dados da Entrega</Text>
-
-                    <View style={{ flexDirection: "row", gap: 20, flexWrap: "wrap" }}>
-
-                        <Text style={styles.label}>O pedido será: <Text style={styles.data}>{order?.tipoEntrega === "ENTREGA" ? "Entrega" : "Retirada"}</Text></Text>
-
-                        <Text style={styles.label}>Data: <Text style={styles.data}>{formatDate(order?.dataPrevisaoEntrega)}</Text></Text>
+                            <Text style={[styles.label, { width: "40%" }]}>Recheio: <Text style={styles.data}>{order?.bolo?.recheioPedido?.sabor1} e {order?.bolo?.recheioPedido?.sabor2}</Text></Text>
+                        </View>
 
                     </View>
 
                     <View>
 
-                        <Text style={styles.subtitle}>Dados do Solicitante</Text>
+                        <Text style={styles.title}>Decoração</Text>
 
-                        <View style={{ gap: 10 }}>
+                        <View>
+                            <Text style={styles.data}>Nenhuma imagem de referência adicionada</Text>
 
-                            <Text style={styles.label}>Nome do solicitante: <Text style={styles.data}>{order?.nomeCliente}</Text></Text>
+                            <Text style={[styles.label, { paddingTop: 20 }]}>Observações</Text>
 
-                            <Text style={styles.label}>Telefone: <Text style={styles.data}>{order?.telefoneCliente}</Text></Text>
+                            <Text style={styles.data}>{order?.observacao}</Text>
 
                         </View>
 
@@ -106,52 +92,90 @@ const OrderSummary = ({ onClose, order }) => {
 
                     <View>
 
-                        <Text style={styles.subtitle}>Endereço</Text>
+                        <Text style={styles.title}>Adicionais</Text>
 
-                        <View style={{ flexDirection: "row", gap: 30, flexWrap: "wrap", width: "80%" }}>
+                        <View>
+                            <Text style={styles.data}>Checks</Text>
+                        </View>
 
-                            <View>
-                                <Text style={styles.label}>CEP</Text>
-                                <Text style={styles.data}>{order?.endereco?.cep}</Text>
+                    </View>
+
+                    <View>
+
+                        <Text style={styles.title}>Dados da Entrega</Text>
+
+                        <View style={{ flexDirection: "row", gap: 20, flexWrap: "wrap" }}>
+
+                            <Text style={styles.label}>O pedido será: <Text style={styles.data}>{order?.tipoEntrega === "ENTREGA" ? "Entrega" : "Retirada"}</Text></Text>
+
+                            <Text style={styles.label}>Data: <Text style={styles.data}>{formatDate(order?.dataPrevisaoEntrega)}</Text></Text>
+
+                        </View>
+
+                        <View>
+
+                            <Text style={styles.subtitle}>Dados do Solicitante</Text>
+
+                            <View style={{ gap: 10 }}>
+
+                                <Text style={styles.label}>Nome do solicitante: <Text style={styles.data}>{order?.nomeCliente}</Text></Text>
+
+                                <Text style={styles.label}>Telefone: <Text style={styles.data}>{order?.telefoneCliente}</Text></Text>
+
                             </View>
 
-                            <View>
-                                <Text style={styles.label}>Estado</Text>
-                                <Text style={styles.data}>{order?.endereco?.estado}</Text>
-                            </View>
+                        </View>
 
-                            <View>
-                                <Text style={styles.label}>Cidade</Text>
-                                <Text style={styles.data}>{order?.endereco?.cidade}</Text>
-                            </View>
+                        <View>
 
-                            <View>
-                                <Text style={styles.label}>Bairro</Text>
-                                <Text style={styles.data}>{order?.endereco?.bairro}</Text>
-                            </View>
+                            <Text style={styles.subtitle}>Endereço</Text>
 
-                            <View>
-                                <Text style={styles.label}>Rua</Text>
-                                <Text style={[styles.data, { width: "70%" }]}>{order?.endereco?.logradouro}</Text>
-                            </View>
+                            <View style={{ flexDirection: "row", gap: 30, flexWrap: "wrap", width: "80%" }}>
 
-                            <View>
-                                <Text style={styles.label}>Número</Text>
-                                <Text style={styles.data}>{order?.endereco?.numero}</Text>
-                            </View>
+                                <View>
+                                    <Text style={styles.label}>CEP</Text>
+                                    <Text style={styles.data}>{order?.endereco?.cep}</Text>
+                                </View>
 
-                            <View>
-                                <Text style={styles.label}>Complemento</Text>
-                                <Text style={styles.data}>{order?.endereco?.complemento || "N/A"}</Text>
+                                <View>
+                                    <Text style={styles.label}>Estado</Text>
+                                    <Text style={styles.data}>{order?.endereco?.estado}</Text>
+                                </View>
+
+                                <View>
+                                    <Text style={styles.label}>Cidade</Text>
+                                    <Text style={styles.data}>{order?.endereco?.cidade}</Text>
+                                </View>
+
+                                <View>
+                                    <Text style={styles.label}>Bairro</Text>
+                                    <Text style={styles.data}>{order?.endereco?.bairro}</Text>
+                                </View>
+
+                                <View>
+                                    <Text style={styles.label}>Rua</Text>
+                                    <Text style={[styles.data, { width: "70%" }]}>{order?.endereco?.logradouro}</Text>
+                                </View>
+
+                                <View>
+                                    <Text style={styles.label}>Número</Text>
+                                    <Text style={styles.data}>{order?.endereco?.numero}</Text>
+                                </View>
+
+                                <View>
+                                    <Text style={styles.label}>Complemento</Text>
+                                    <Text style={styles.data}>{order?.endereco?.complemento || "N/A"}</Text>
+                                </View>
+
                             </View>
 
                         </View>
 
                     </View>
 
-                </View>
+                </ScrollView>
 
-            </ScrollView>
+            </Animated.View>
 
         </View>
 
